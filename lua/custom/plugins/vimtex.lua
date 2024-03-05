79c0b1040443e9395e60426c7a810ac7da72b6bb
samuelqp <119834862+samuelqp@users.noreply.github.com>
2024-03-04 22:24:48 -0500

    Add vimtex plugin

---
+++ lua/custom/plugins/vimtex.lua
@@ -0,0 +1,14 @@
+return{
+  'lervag/vimtex',
+  init = function()
+    -- Use init for configuration, don't use the more common "config".
+    vim.g.vimtex_view_method = "zathura_simple"
+    vim.g.vimtex_view_general_viewer = 'zathura_simple'
+    vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.tex_flavor = 'latex'
+
+    vim.cmd([[let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex']])
+    vim.cmd([[filetype plugin indent on]])
+    vim.cmd([[syntax enable]])
+  end
+}
