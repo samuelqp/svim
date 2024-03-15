0754368042d19f654a22c0be31e04db82e45e4a7
Samuel Quinteiro Perez <119834862+samuelqp@users.noreply.github.com>
2024-03-14 21:33:03 -0400

    Remove incompatible snippets

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -52,7 +52,7 @@ require('lazy').setup({
       'hrsh7th/cmp-path',
 
       -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+      -- 'rafamadriz/friendly-snippets',
     },
   },
 
