2f5e00f0405cd32b51a1b8b5f2b5265a2d228a3e
samuelqp <119834862+samuelqp@users.noreply.github.com>
2024-03-15 12:46:22 -0400

    Indicate python path

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.python3_host_prog = '/usr/bin/python3'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
